ca7e10085114f950041f806ca2f6f8221f9644eb
beesuut <jdcheng06@gmail.com>
2026-03-01 23:41:06 -0800

    fix cors vs frontend url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
